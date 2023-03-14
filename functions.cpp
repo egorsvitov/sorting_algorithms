13ad1d405f6b53c3b68dc054f91239fe5388332f
egor <svitov.ev@phystech.edu>
2023-03-14 20:35:39 +0300

    init

---
+++ functions.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <numeric>
+
+void make_a_frame(int* array, int array_size, int max_elem) {
+    int width = 1920 / array_size;
+    int height = 1080 / max_elem;
+
+    sf::Image background;
+    background.create(1920, 1080, sf::Color::Black); 
+    for (int i = 0; i < array_size; i++) {
+        sf::Color column_color{255, 255, 255};
+        sf::Image column;
+        column.create(width, array[i]*height, column_color);
+        background.copy(column, width*i, (1080 - array[i]*height));
+    }
+    background.saveToFile("test.png");
+}
+
+int* make_shuffled_array(int size) {
+    std::vector<int> array_v(size);
+    std::iota(array_v.begin(), array_v.end(), 1);
+    std::random_shuffle(array_v.begin(), array_v.end());
+    int* array = new int[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = array_v[i];
+    }
+    return array;
+}
---
+++ functions.hpp
@@ -0,0 +1,7 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+void make_a_frame(int*, int, int);
+int* make_shuffled_array(int);
+
+#endif
