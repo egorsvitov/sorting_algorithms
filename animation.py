211d3ca4de1840635075300bb3dd9211a24ff439
egor <svitov.ev@phystech.edu>
2023-03-16 15:29:06 +0300

    final

--- animation.py
+++ animation.py
@@ -4,9 +4,8 @@ filenames = []
 for i in range(1, 2390):
     filenames.append(f"/home/egor/frames/frame_{i}.png")
 
-with imageio.get_writer('/home/egor/bubble_sort.mp4') as writer:
+with imageio.get_writer('/home/egor/bubble_sort.mp4', fps=300) as writer:
     for i, filename in enumerate(filenames):
         print(i)
         image = imageio.imread(filename)
         writer.append_data(image)
-
---
+++ main.cpp
@@ -0,0 +1,4 @@
+#include "functions.hpp"
+
+int main() {
+}
